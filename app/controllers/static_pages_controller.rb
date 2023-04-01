360ac3a1c76774b17396efceeed66b9a8925597c
nfrank5 <nfrankenfeld@gmail.com>
2023-04-01 09:51:46 +0800

    Functioning

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,10 @@ class StaticPagesController < ApplicationController
  
   def home
     flickr = Flickr.new Figaro.env.pusher_flickr_key, Figaro.env.pusher_flickr_secret
-    @list = flickr.photos.getRecent
+    begin
+      @list = flickr.people.getPublicPhotos(api_key: Figaro.env.pusher_flickr_key, user_id: params[:flickr_user_id])
+    rescue
+      @list = []
+    end
   end
 end
